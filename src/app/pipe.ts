797ef8ba7b59aedcced15a9e704ad1df6629e21e
jasmin-8076 <111449105+jasmin-8076@users.noreply.github.com>
2023-02-01 22:29:30 +0530

    student data added and displayed on ui

---
+++ src/app/pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Student} from './data';
+
+@Pipe({ name: 'students' })
+export class CountryPipe implements PipeTransform {
+  transform(values: Student[], filter: string): Student[] {
+    if (!filter || filter.length === 0) {
+      return values;
+    }
+
+    if (values.length === 0) {
+      return values;
+    }
+
+    return values.filter((value: Student) => {
+      const nameFound =
+        value.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+      const genderFound =
+        value.gender.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+
+      if (nameFound || genderFound) {
+        return value;
+      }
+    });
+  }
+}
